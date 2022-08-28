d0fa57ae74743a02b606db51b04e86d44d666edb
Jake Busby <jdbusby@mc.edu>
2022-08-28 17:54:43 -0500

    First Commit

---
+++ js/main.js
@@ -0,0 +1,143 @@
+
+
+
+// Create the application helper and add its render target to the page
+let app = new PIXI.Application({ width: 1600, height: 1600, transparent: false, antialis: true });
+document.body.appendChild(app.view);
+
+// Create the sprite and add it to the stage
+const loader = PIXI.Loader.shared;
+
+loader.add('tileset', 'Borg.json')
+loader.add('atlas', 'atlas.json');
+//loader.add('assets','assets/maps/key.json');
+
+let test = new TileMap(app);
+
+console.log(test)
+
+var borgSprite;
+
+
+loader.load(loadChar);
+
+
+// Listen for animate update
+app.ticker.add((delta) => {
+    // rotate the container!
+    // use delta to create frame-independent transform
+    //container.rotation -= 0.01 * delta;
+
+
+});
+
+function loadChar(loader, resources) {
+    console.log(resources)
+    const textures = [];
+    //11 17 23
+
+    for (let i = 0; i < 5; i++) {
+        if (i != 5) {
+            if (i != 11) {
+                if (i != 17) {
+                    if (i != 23) {
+                        const texture = PIXI.Texture.from(i + ".png");
+                        textures.push(texture);
+                    }
+                }
+
+
+            }
+
+
+        }
+    }
+    //const borgTexture = PIXI.Texture.from('00.png');
+    borgSprite = new PIXI.AnimatedSprite(textures);
+    borgSprite.position.set(test.width / 2, test.height / 2);
+    test.current_position = {"col":10,"row":15};
+    test.mapArr[10][15].char_here = true;
+
+    //borgSprite.scale.set(.8, .8);
+
+    app.stage.addChild(borgSprite);
+
+    borgSprite.play();
+    borgSprite.animationSpeed = 0.1;
+
+    app.stage.interactive = true;
+
+    app.stage.on('click',function(e){
+        console.log(e.data.global)
+    });
+}
+
+function moveChar(e) {
+    //console.log('e',e)
+    //Move Left
+    if (e.keyCode == 65) {
+        if(check_if_walkable(e.keyCode,{"col" : test.current_position.col, "row":test.current_position.row - 1})){
+            test.updateCharPosition({"col" : test.current_position.col, "row":test.current_position.row - 1})
+            borgSprite.position.set(borgSprite.position._x - 16, borgSprite.position._y)
+            //test.current_position = {"col" : test.current_position.col, "row":test.current_position.row - 1}
+            console.log(test.current_position)
+        
+        }
+        
+
+    }
+    //Move Right
+    else if (e.keyCode == 68) {
+        if(check_if_walkable(e.keyCode,{"col" : test.current_position.col, "row":test.current_position.row + 1})){
+            test.updateCharPosition({"col" : test.current_position.col, "row":test.current_position.row + 1})
+            borgSprite.position.set(borgSprite.position._x + 16, borgSprite.position._y)
+            //test.current_position = {"col" : test.current_position.col, "row":test.current_position.row - 1}
+            console.log(test.current_position)
+        
+        }
+        
+    }
+    //Move Down
+    else if (e.keyCode == 83) {
+        if(check_if_walkable(e.keyCode,{"col" : test.current_position.col + 1, "row":test.current_position.row})){
+            test.updateCharPosition({"col" : test.current_position.col + 1, "row":test.current_position.row})
+            borgSprite.position.set(borgSprite.position._x, borgSprite.position._y + 16)
+            //test.current_position = {"col" : test.current_position.col, "row":test.current_position.row - 1}
+            console.log(test.current_position)
+        
+        }
+        
+    }
+    else if (e.keyCode == 87) {
+        if(check_if_walkable(e.keyCode,{"col" : test.current_position.col - 1, "row":test.current_position.row})){
+            test.updateCharPosition({"col" : test.current_position.col -1 , "row":test.current_position.row})
+            borgSprite.position.set(borgSprite.position._x, borgSprite.position._y - 16)
+            //test.current_position = {"col" : test.current_position.col, "row":test.current_position.row - 1}
+            console.log(test.current_position)
+        
+        }
+        
+    }
+}
+
+function check_if_walkable(keyCode,position){
+    //let current_position = test.find_char_position();
+    if(position.col > -1 && position.row > -1)
+
+    if(test.mapArr[position.col][position.row].walkable){
+        console.log(true)
+        return true;
+    }
+    else{
+        console.log(false)
+        return false;
+    }
+    
+    
+}
+
+$('body').on('keydown',moveChar);
+
+
+
+console.log(loader)
---
+++ js/tilemap.js
@@ -0,0 +1,170 @@
+// map
+class TileMap {
+    constructor(game) {
+
+        this._game = game;
+        this._body = {};
+        this.width;
+        this.height;
+        this.collisionLayer;
+        this.mapArr;
+        this.current_position = {"col":0,"row":0};
+        var tileset = PIXI.Texture.from("/assets/maps/map.png");
+        console.log('tileset', tileset)
+        //this._game.on( 'update', this._update.bind( this ) );
+        this._createMap(tileset);
+    }
+
+    async _createMap(tileset) {
+
+        // load json
+        //var level = require('../map/map.json');j
+        var level = await this.getJsonFile(function (data) {
+            return data
+        });
+        console.log(level)
+        var tileHeight = 16;
+        var tileWidth = 16;
+        var layers = level.layers;
+        var height = level.height;
+        var width = level.width;
+
+        this.width = width * tileWidth;
+        this.height = height * tileHeight;
+
+        let levelHeight = 0;
+        console.log(level)
+
+        this.collisionLayer = this.getCollisionLayer(layers);
+        console.log(this.collisionLayer)
+
+        this.createMapArray(this.collisionLayer)
+
+
+        for (let col = 0; col < height; col++) {
+            for (let row = 0; row < width; row++) {
+                //Postition on screen
+                //height = 20
+                //width = 30
+                if (row % (width - 1) == 0) {
+                    if (row > 0) {
+                        var y = levelHeight;
+                        var x = row % width | 0;
+                        levelHeight++;
+                    }
+                    else {
+                        var y = levelHeight;
+                        var x = row % width | 0;
+                    }
+
+                }
+                else {
+                    var y = levelHeight;
+                    var x = row % width | 0;
+                }
+
+                //         console.log("data",data[i])
+
+
+                //I know this looks backwards, but trust :)
+                var tileRow = col;
+                var tileCol = row;
+
+                // console.log('tileRow', row);
+                // console.log('tileCol', col);
+                // console.log('')
+                //x and y
+                var text = new PIXI.Texture(tileset, new PIXI.Rectangle(tileCol * 16, tileRow * 16, tileWidth, tileHeight));
+                var layer = new PIXI.Sprite(text);
+                layer.x = x * tileHeight;
+                layer.y = y * tileWidth;
+                // console.log('layer x',layer.x);
+                // console.log('layer y',layer.y);
+                // console.log('')
+                this._game.stage.addChild(layer);
+
+                //if(row == 30){throw new Error("Something went badly wrong!");}
+
+            }//row
+            //if(col == 1){throw new Error("Something went badly wrong!");}
+        }
+        // }
+    }
+
+    createMapArray(collisionLayer){
+        this.mapArr = [];
+        let index = 0;
+
+        for(let col =0; col < collisionLayer.height; col++){
+            let tempArr = [];
+            for(let row =0; row < collisionLayer.width; row++){
+                if(collisionLayer.data[index] == 0){
+                    tempArr.push({"walkable" : true, "char_here": false})
+                }
+                else{
+                    //this.mapArr[col][row] = 1
+                    tempArr.push({"walkable" : false, "char_here": false})
+                }
+                index++;
+            }
+            this.mapArr.push(tempArr)
+        }
+
+        console.log(this.mapArr)
+    }
+
+    find_char_position(){
+        for(let col = 0; col < this.collisionLayer.height; col++){
+            for(let row = 0; row < this.collisionLayer.width; row++){
+                if(this.mapArr[col][row].char_here){
+                    console.log({"col":col, "row":row})
+                    return {"col":col, "row":row};
+                }
+            }
+        }
+
+        return null;
+    }
+
+    getCollisionLayer(mapData) {
+
+        for (let i = 0; i < mapData.length; i++) {
+
+            if (mapData[i].name == "Collision") {
+                return mapData[i];
+            }
+
+        }
+
+        return null;
+    }
+
+    updateCharPosition(position){
+        this.mapArr[this.current_position.col][this.current_position.row].char_here = false;
+        this.mapArr[position.col][position.row].char_here = true;
+        this.current_position = {"col" : position.col, "row" : position.row}
+    }
+
+    async getJsonFile(callback) {
+        // http://localhost:8080
+        let file = await fetch('/assets/maps/map.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("HTTP error " + response.status);
+                }
+                return response.json();
+            })
+            .then(json => {
+                //console.log(json)
+                return json;
+                //callback(json);
+                //console.log(this.users);
+            })
+            .catch(function () {
+                console.log('error')
+            })
+
+        return file;
+    }
+
+};
